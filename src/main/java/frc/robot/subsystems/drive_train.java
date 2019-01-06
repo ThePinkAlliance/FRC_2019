8d7953a214fe493d6df7949dfe611aaf0a6d89fe
AidanF <aidanf2020@gmail.com>
2019-01-06 10:29:25 -0500

    Drive Train Subsystem
    Some method layout for the drive train.

--- src/main/java/frc/robot/subsystems/drive_train.java
+++ src/main/java/frc/robot/subsystems/drive_train.java
@@ -1,20 +1,25 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * An example subsystem.  You can replace me with your own Subsystem.
- */
+
+// A subsystem for control of the drive train.
 public class drive_train extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+  
+  // A method for driving using a command. (From -1 to 1)
+  public void drive_by_command() {
+    
+  }
+
+  // A method for driving a certain distance. (In inches)
+  public void drive_to_distance() {
+
+  }
+
+  // A method for turning to a position based off initial value. (From -180 to 180)
+  public void turn_to_position() {
+
+  }
 
   @Override
   public void initDefaultCommand() {
--- src/main/java/frc/robot/subsystems/hatch.java
+++ src/main/java/frc/robot/subsystems/hatch.java
@@ -1,17 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * An example subsystem.  You can replace me with your own Subsystem.
- */
+
+// A subsystem for control of the hatch subsystem. (Collecting and Scoring)
 public class hatch extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
